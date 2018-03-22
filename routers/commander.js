cb8e3cebe12c4604e5c1e055c22574807895f1a2
Kévin <kevin.lecalvez3@gmail.com>
2018-03-22 16:24:35 +0100

    Persistances des commandes

--- routers/commander.js
+++ routers/commander.js
@@ -3,6 +3,11 @@ const commanderRouter = express.Router()
 const bodyParser = require('body-parser')
 const path = require('path')
 const tools = require('../tools/toolsCommandes')
+const { Readable, Writable, Transform } = require('stream')
+const { promisify } = require('util')
+const readline = require('readline')
+const fs = require('fs')
+const promiseFsWriteFile = promisify(fs.writeFile)
 
 var produits = tools.GetProduits()
 var commandes = tools.GetCommandes()
@@ -43,6 +48,33 @@ function validateCommandeData(req, res, next) {
     }
 }
 
+function enregistrementJSON(commande){
+    console.log('on passe dans l\'enregisteur : '+ commande.id)
+   /* const enregistrementCommande = new Writable({
+        objectMode: true,
+      
+        write(commande, encoding, next) {
+          const filepath = path.join(
+            __dirname,
+            `commande_${commande.id}.json`
+          )
+
+          console.log('fichier créer : ' + filepath)
+      
+          //JSON.stringify(commande))
+          promiseFsWriteFile(filepath, 'coucou')
+            .then(() => next())
+        }
+    });*/
+    let pathDir = path.join('commandes', 'Refsnes', '..',  'commande_'+ commande.id + '.json');
+    var wstream = fs.createWriteStream(pathDir);
+    promiseFsWriteFile(pathDir, JSON.stringify(commande))
+            .then(() => next())
+    //wstream.write(commande);
+    // on Node.js older than 0.10, add cb to end()
+    wstream.end(function () { console.log('Fichier enregistré : ' + pathDir); });
+}
+
 // lectures
 commanderRouter.get('/', (req, res) => res.sendFile(path.join(__dirname , 'commander.html')))
 commanderRouter.get('../', (req,res) => res.redirect('localhost:3000/'))
@@ -52,6 +84,7 @@ commanderRouter.post('/', validateCommandeData, (req, res) => {
     const commande = Object.assign({ id: tools.GetNextCommandeId() }, req.commandeData)
     tools.AddCommande(commande)
     console.log(commandes)
+    enregistrementJSON(commande)
     res.sendFile(path.join(__dirname , 'valider.html'))
 })
 
